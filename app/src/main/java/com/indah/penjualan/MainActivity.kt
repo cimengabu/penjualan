27025f7500f25f74feb0ae54dbcec5f22b5b394f
indah <Ind4hwijaya06@gmail.com>
2026-01-16 00:31:19 +0700

    strings

--- app/src/main/java/com/indah/penjualan/MainActivity.kt
+++ app/src/main/java/com/indah/penjualan/MainActivity.kt
@@ -1,20 +1,35 @@
-package com.indah.penjualan
+package com.indah.penjualan // Sesuaikan dengan package name anda
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Inisialisasi CardView berdasarkan ID di XML
+        val cardProfil = findViewById<CardView>(R.id.card2)
+        val cardProduk = findViewById<CardView>(R.id.card3)
+        val cardKategori = findViewById<CardView>(R.id.card4)
+        val cardPegawai = findViewById<CardView>(R.id.card5)
+        val cardCabang = findViewById<CardView>(R.id.card6)
+        val cardPrinter = findViewById<CardView>(R.id.card7)
+
+        // Contoh Memberikan Aksi Klik
+        cardProfil.setOnClickListener {
+            // Mengambil teks dari strings.xml lewat kode Kotlin
+            val pesan = getString(R.string.profil)
+            Toast.makeText(this, "Membuka Menu $pesan", Toast.LENGTH_SHORT).show()
         }
+
+        cardProduk.setOnClickListener {
+            Toast.makeText(this, "Membuka Menu Produk", Toast.LENGTH_SHORT).show()
+        }
+
+        // Tambahkan aksi untuk card lainnya di sini...
     }
 }
